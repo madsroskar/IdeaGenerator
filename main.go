1ae94db142e6d0c76de73402dda9c9eb58ea4b3d
Mads Røskar <madshvero@gmail.com>
2018-06-06 16:42:23 +0200

    Separate idea to a separate file

--- main.go
+++ main.go
@@ -2,45 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"strings"
 	"math/rand"
 	"time"
 )
 
 func random(min, max int) int {
 	return rand.Intn(max - min) + min
-}
-
-type Idea struct {
-	source string
-	target string
-}
-
-func (i Idea) String() string {
-	return fmt.Sprintf("%s, but for %s", i.source, i.target)
-}
-
-func createIdea(sources, targets []byte) Idea {
-	trimmedSources := strings.TrimSpace(string(sources))
-	trimmedTargets := strings.TrimSpace(string(targets))
-	
-	s := strings.Split(trimmedSources, "\n")
-	t := strings.Split(trimmedTargets, "\n")
-	
-	return Idea{s[random(0, len(s))], t[random(0, len(t))]}
-}
-
-func readFile(fileName string) []byte {
-	content, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		fmt.Print(err)
-	}
-	return content
-}
-
-func readFiles(sourceFileName, targetFileName string) ([]byte, []byte) {
-	return readFile(sourceFileName), readFile(targetFileName)
 }
 
 func main() {
