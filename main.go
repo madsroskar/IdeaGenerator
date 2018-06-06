2e29bbdee9264e3512e6071628481063b903ea4e
Mads Røskar <madshvero@gmail.com>
2018-06-06 17:07:04 +0200

    Make a simple API returning the ideas

--- main.go
+++ main.go
@@ -4,16 +4,29 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"encoding/json"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
 )
+
+var sources, targets []byte = readFiles("existing-idea.txt", "targets.txt")
 
 func random(min, max int) int {
 	return rand.Intn(max - min) + min
 }
 
+func getIdea(w http.ResponseWriter, r *http.Request) {
+	i := make(map[string]string)
+	i["Idea"] = fmt.Sprintf("%s", createIdea(sources, targets))
+	json.NewEncoder(w).Encode(i)
+}
+
 func main() {
 	rand.Seed(time.Now().Unix())
-	var sources, targets []byte = readFiles("existing-idea.txt", "targets.txt")
-	idea := createIdea(sources, targets)
-	fmt.Println(idea)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", getIdea).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
