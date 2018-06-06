ab7286c7f29f21b7b6a7b8edf8350b46d161ce6e
Mads Røskar <madshvero@gmail.com>
2018-06-06 21:58:25 +0200

    Add super simple html rendering

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"path/filepath"
+	"os"
 	"fmt"
 	"math/rand"
 	"time"
-	"encoding/json"
+	//"encoding/json"
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+    "html/template"
 )
 
 var sources, targets []byte = readFiles("existing-idea.txt", "targets.txt")
@@ -17,15 +20,32 @@ func random(min, max int) int {
 }
 
 func getIdea(w http.ResponseWriter, r *http.Request) {
-	i := make(map[string]string)
-	i["Idea"] = fmt.Sprintf("%s", createIdea(sources, targets))
-	json.NewEncoder(w).Encode(i)
+	//i := make(map[string]string)
+	//i["Idea"] = fmt.Sprintf("%s", createIdea(sources, targets))
+	//json.NewEncoder(w).Encode(i)
+
+
+    t, err := template.ParseFiles("./tmpl/idea.html")  // Parse template file.
+    if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(t)
+	}
+	idea := createIdea(sources, targets) // Get current user infomration.
+    t.Execute(w, idea)  // merge.
 }
 
 func main() {
 	rand.Seed(time.Now().Unix())
+    
+	_, err := os.Stat(filepath.Join(".", "tmpl", "css", "style.css"))
+
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	router := mux.NewRouter()
+	router.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("./tmpl/css/"))))
 	router.HandleFunc("/", getIdea).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
