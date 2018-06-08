7eacd88f1f75f639b35b96050e5588e249e72b14
Mads Røskar <madshvero@gmail.com>
2018-06-08 09:16:38 +0200

    Move idea controller info into its own file

--- src/main.go
+++ src/main.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
 	"time"
 )
 
@@ -20,41 +19,7 @@ func random(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
-func getRandomIdea(w http.ResponseWriter, r *http.Request) {
-	v := r.Header.Get("Content-Type")
-	var page Page = Page{createRandomIdea(sources, targets), "idea"}
-	if v == "application/json" {
-		page.renderJson(w)
-		return
-	}
-	page.renderHtml(w)
-}
 
-func getIdea(w http.ResponseWriter, r *http.Request) {
-	v := r.Header.Get("Content-Type")
-	vars := mux.Vars(r)
-
-	source, serr := strconv.Atoi(vars["source"])
-	target, terr := strconv.Atoi(vars["target"])
-
-	if serr != nil || terr != nil {
-		return
-	}
-
-	idea, err := createIdea(sources, targets, source, target)
-
-	if err != nil {
-		http.NotFound(w, r)
-		return
-	}
-
-	var page Page = Page{idea, "idea"}
-	if v == "application/json" {
-		page.renderJson(w)
-		return
-	}
-	page.renderHtml(w)
-}
 
 func main() {
 	rand.Seed(time.Now().Unix())
